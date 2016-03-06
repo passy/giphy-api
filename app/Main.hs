c2931ae9cfec65e10d3f2aad6f5479885abae5c8
Pascal Hartig <phartig@rdrei.net>
2016-03-06 11:56:58 +0000

    CLI parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,47 @@
+{-# LANGUAGE OverloadedStrings          #-}
+
 module Main where
 
-import Lib
+import qualified Data.Text as T
+import qualified Options.Applicative as Opt
+
+import           Data.Version             (Version (), showVersion)
+import Control.Applicative ((<**>))
+import Paths_givegif (version)
+
+import Data.Monoid ((<>))
+import qualified Web.Giphy.Search as Giphy
+
+data Options = Options
+  { query :: T.Text
+  }
+
+apiKey :: Giphy.Key
+apiKey = Giphy.Key "dc6zaTOxFJmzC"
+
+options :: Opt.Parser Options
+options = Options . T.pack <$> Opt.argument Opt.str
+                             ( Opt.metavar "QUERY"
+                            <> Opt.help "Search query for your GIFs" )
+
+cliParser :: Version -> Opt.ParserInfo Options
+cliParser ver =
+  Opt.info ( Opt.helper <*> options <**> versionInfo )
+    ( Opt.fullDesc
+   <> Opt.progDesc "Find GIFs on the command line."
+   <> Opt.header "givegif" )
+  where
+    versionInfo = Opt.infoOption ( "givegif " <> showVersion ver )
+      ( Opt.short 'V'
+     <> Opt.long "version"
+     <> Opt.hidden
+     <> Opt.help "Show version information" )
 
 main :: IO ()
-main = someFunc
+main = Opt.execParser (cliParser version) >>= run
+  where
+    run :: Options -> IO ()
+    run opts = do
+      -- TODO: Lenses. For everything!
+      resp <- Giphy.search apiKey (Giphy.Query $ query opts)
+      print resp
