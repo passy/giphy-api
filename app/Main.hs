6629250ce7d7bf21961fe08d676f5aba76e1de8a
Pascal Hartig <phartig@rdrei.net>
2016-03-14 18:10:14 +0000

    WIP: Translate CLI

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import qualified Data.Text              as T
 import qualified Options.Applicative    as Opt
 import qualified Web.Giphy              as Giphy
+import qualified Network.URI            as URI
 
 import           Control.Applicative    ((<**>))
 import           Data.Version           (Version (), showVersion)
@@ -16,9 +17,11 @@ import           Control.Lens.Operators
 import           Control.Lens.Prism     (_Right)
 import           Control.Lens.Cons      (_head)
 import           Control.Lens.At        (at)
+import Control.Monad (join)
 
 data Options = Options
   { query :: T.Text
+  , useTranslate :: Bool
   }
 
 apiKey :: Giphy.Key
@@ -28,6 +31,10 @@ options :: Opt.Parser Options
 options = Options . T.pack <$> Opt.argument Opt.str
                              ( Opt.metavar "QUERY"
                             <> Opt.help "Search query for your GIFs" )
+                           <*> Opt.switch
+                             ( Opt.long "translate"
+                            <> Opt.short 't'
+                            <> Opt.help "Use translate instead of search" )
 
 cliParser :: Version -> Opt.ParserInfo Options
 cliParser ver =
@@ -59,3 +66,31 @@ main = Opt.execParser (cliParser version) >>= run
                          . Giphy.imageUrl
                          . traverse
       print fstUrl
+
+translateApp :: T.Text -> Giphy.Giphy (Maybe URI.URI)
+translateApp q = do
+  let phrase = Giphy.Phrase q
+  resp <- Giphy.translate phrase
+
+  return $ resp ^? _Right
+                 . Giphy.translateItems
+                 . _head
+                 . Giphy.gifImages
+                 . at "original"
+                 . traverse
+                 . Giphy.imageUrl
+                 . traverse
+
+searchApp :: T.Text -> Giphy.Giphy (Maybe URI.URI)
+searchApp q = do
+  let phrase = Giphy.Phrase q
+  resp <- Giphy.translate phrase
+
+  return $ resp ^? _Right
+                 . Giphy.searchItems
+                 . _head
+                 . Giphy.gifImages
+                 . at "original"
+                 . traverse
+                 . Giphy.imageUrl
+                 . traverse
